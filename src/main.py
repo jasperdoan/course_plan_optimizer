097eafe4acfcc1b7e91f5ce9ae31f8936639857d
Jasper <49565505+jasperdoan@users.noreply.github.com>
2023-07-14 09:21:31 -0700

    dag approach

--- src/main.py
+++ src/main.py
@@ -1,23 +1,19 @@
 import pandas as pd
 from utils import (
-    read_csv_to_dict, 
-    prereq_tree,
-    course_option_tree,
-    graph_prereq_relationship,
+    read_csv_to_dict,
+    prereq_adjlist,
+    create_dag,
+    level_order_traversal,
+    graph_relationship,
     topological_sort
 )
 
 def main():
     course_dict = read_csv_to_dict('data\courses.csv')
-    adj_list = prereq_tree(course_dict)
-    # graph_prereq_relationship(adj_list)
+    adj_list = prereq_adjlist(course_dict)
+    dag = create_dag(course_dict)
 
-    course_tree = course_option_tree(adj_list)
-    sorted_list = topological_sort(course_tree)
-
-
-    for i in range(0, len(sorted_list), 4):
-        print(sorted_list[i:i+4])
+    print(adj_list)
 
     
 
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
+from collections import deque
 
 def read_csv_to_dict(path: str) -> dict:
     df = pd.read_csv(path)
@@ -17,7 +18,7 @@ def read_csv_to_dict(path: str) -> dict:
     return course_dict
 
 
-def prereq_tree(course_dict: dict) -> dict:
+def prereq_adjlist(course_dict: dict) -> dict:
     adj_list = {}
 
     for course_id, (_, prerequisites_list, _) in course_dict.items():
@@ -26,25 +27,53 @@ def prereq_tree(course_dict: dict) -> dict:
     return adj_list
 
 
-def course_option_tree(adj_list: dict) -> dict:
-    course_tree = adj_list.copy()        
+def create_dag(course_dict: dict) -> dict:
+    dag = {}
 
-    for course, prereq_list in adj_list.items():
-        course_tree[course] = []
-        for prereq in prereq_list:
-            course_tree[prereq].append(course)
-        
-    return course_tree
+    for course_id, (_, prerequisites_list, _) in course_dict.items():
+        dag[course_id] = []
+        for prereq in prerequisites_list:
+            dag[prereq].append(course_id)
+
+    return dag
 
 
-def graph_prereq_relationship(adj_list: dict) -> None:
+def level_order_traversal(adj_list: dict) -> list:
+    dags = []
+    visited = set()
+
+    def bfs(course):
+        dag = {}
+        queue = deque([(course, 0)])
+
+        while queue:
+            current, level = queue.popleft()
+
+            if current not in dag:
+                dag[current] = level
+            visited.add(current)
+
+            for prerequisite in adj_list[current]:
+                if prerequisite not in visited:
+                    queue.append((prerequisite, level + 1))
+
+        return dag
+
+    for course in adj_list:
+        if course not in visited:
+            dag = bfs(course)
+            dags.append(dag)
+
+    return dags
+
+
+def graph_relationship(dag: dict) -> None:
     G = nx.Graph()
+    G.add_nodes_from(dag.keys())
 
-    G.add_nodes_from(adj_list.keys())
-
-    for node, prerequisites_list in adj_list.items():
-        for prerequisite in prerequisites_list:
-            G.add_edge(node, prerequisite)
+    for n, edges in dag.items():
+        for e in edges:
+            G.add_edge(n, e)
 
     # Set node spacing options
     layout_options = {'k': .5, 'iterations': 50}
@@ -64,7 +93,20 @@ def graph_prereq_relationship(adj_list: dict) -> None:
     plt.show()
 
 
-def topological_sort(adj_list):
-    graph = nx.DiGraph(adj_list)
-    sorted_classes = list(nx.topological_sort(graph))
-    return sorted_classes
+def topological_sort(dag: dict) -> list:
+    def dfs(course):
+        visited.add(course)
+        for prerequisite in dag[course]:
+            if prerequisite not in visited:
+                dfs(prerequisite)
+        topological_order.append(course)
+
+    visited = set()
+    topological_order = []
+
+    for course in dag:
+        if course not in visited:
+            dfs(course)
+
+    topological_order.reverse()
+    return topological_order
