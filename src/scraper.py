fb5d072ceee2fac9d8b0ac7cb5e6da04d4ff541f
Jasper <49565505+jasperdoan@users.noreply.github.com>
2023-07-20 04:18:48 -0700

    Working scraper -> For class availability

--- src/scraper.py
+++ src/scraper.py
@@ -9,13 +9,23 @@ html = page.read().decode("utf-8")
 sindex = html.find('["id":protected]=>')
 eindex = html.find('["description":protected]=>' , sindex)
 
+
+dict = {}
+
+
 while True:
     if sindex == -1 or eindex == -1:
         break
+
+    aval_idx = html[sindex:html.find('["cores":protected]=>' , sindex)]
+    SESSIONS = ['Fall', 'Winter', 'Spring']
+    availability = []
+    for session in SESSIONS:
+        if session in aval_idx:
+            availability.append(session)    
     
     info = html[sindex:eindex]
-    # print(info)
-
+    
     course_idx = info.find('string(3) ')
     course_idx = info.find('string(3) ', course_idx + 1)
     course_title = info[course_idx + 11:course_idx + 14]
@@ -26,14 +36,13 @@ while True:
     course_num = course_num.replace(' ', '')
     course_num = course_num.replace('\n', '')
 
-    desc_idx = info.find('string(', cnum_idx + 1)
-    course_desc = info[desc_idx + 12:len(info) - 4]
+    
+    dict[course_title + ' ' + course_num] = availability
 
-    print(f'{course_title} {course_num} {course_desc}')
-
-
-    print('--------------------------')
 
     sindex = html.find('["id":protected]=>', eindex)
     eindex = html.find('["description":protected]=>', sindex)
 
+
+for key, value in dict.items():
+    print(key, value)
