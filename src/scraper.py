876525b7b8c71b65bca62c3d73056a48286dc3d4
Jasper <49565505+jasperdoan@users.noreply.github.com>
2023-07-20 04:33:30 -0700

    scraper class

--- src/scraper.py
+++ src/scraper.py
@@ -1,48 +1,51 @@
 from urllib.request import urlopen
+from dataclasses import dataclass
 
-url = 'https://www.ics.uci.edu/ugrad/courses/listing-course.php?year=2023&level=ALL&department=INF&program=ALL'
+@dataclass
+class AvailabilityScraper:
+    year: int
+    level: str
+    department: str
+    program: str
 
-page = urlopen(url)
-
-html = page.read().decode("utf-8")
-
-sindex = html.find('["id":protected]=>')
-eindex = html.find('["description":protected]=>' , sindex)
-
-
-dict = {}
-
-
-while True:
-    if sindex == -1 or eindex == -1:
-        break
-
-    aval_idx = html[sindex:html.find('["cores":protected]=>' , sindex)]
-    SESSIONS = ['Fall', 'Winter', 'Spring']
-    availability = []
-    for session in SESSIONS:
-        if session in aval_idx:
-            availability.append(session)    
+    @property
+    def course_availability(self) -> dict:
+        return self._course_availability
     
-    info = html[sindex:eindex]
-    
-    course_idx = info.find('string(3) ')
-    course_idx = info.find('string(3) ', course_idx + 1)
-    course_title = info[course_idx + 11:course_idx + 14]
+    def __post_init__(self):
+        self._url = f'https://www.ics.uci.edu/ugrad/courses/listing-course.php?year={self.year}&level={self.level}&department={self.department}&program={self.program}'
+        
+        self._html = urlopen(self._url).read().decode('utf-8')
+        self._sindex = self._html.find('["id":protected]=>')
+        self._eindex = self._html.find('["description":protected]=>', self._sindex)
+        self._course_availability = {}
+        self.__scrape()
 
-    cnum_idx = info.find('string(', course_idx + 1)
-    course_num = info[cnum_idx + 10:cnum_idx + 16]
-    course_num = course_num.replace('"', '')
-    course_num = course_num.replace(' ', '')
-    course_num = course_num.replace('\n', '')
+    def __scrape(self) -> None:
+        while True:
+            if self._sindex == -1 or self._eindex == -1:
+                break
 
-    
-    dict[course_title + ' ' + course_num] = availability
+            aval_idx = self._html[self._sindex:self._html.find('["cores":protected]=>', self._sindex)]
+            SESSIONS = ['Fall', 'Winter', 'Spring']
+            availability = []
+            for session in SESSIONS:
+                if session in aval_idx:
+                    availability.append(session)
 
+            info = self._html[self._sindex:self._eindex]
 
-    sindex = html.find('["id":protected]=>', eindex)
-    eindex = html.find('["description":protected]=>', sindex)
+            course_idx = info.find('string(3) ')
+            course_idx = info.find('string(3) ', course_idx + 1)
+            course_title = info[course_idx + 11:course_idx + 14]
 
+            cnum_idx = info.find('string(', course_idx + 1)
+            course_num = info[cnum_idx + 10:cnum_idx + 16]
+            course_num = course_num.replace('"', '')
+            course_num = course_num.replace(' ', '')
+            course_num = course_num.replace('\n', '')
 
-for key, value in dict.items():
-    print(key, value)
+            self._course_availability[course_title + ' ' + course_num] = availability
+
+            self._sindex = self._html.find('["id":protected]=>', self._eindex)
+            self._eindex = self._html.find('["description":protected]=>', self._sindex)
