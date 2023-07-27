9940540ad01c43c8aa5a946d184c13f934402e61
Jasper <49565505+jasperdoan@users.noreply.github.com>
2023-07-26 23:25:21 -0700

    Cleaned up main & updated course avail

--- src/main.py
+++ src/main.py
@@ -1,91 +1,32 @@
 
 from planner import CoursePlanner
-from scraper import *
+from scraper import read_csv
 from utils import *
-
-
-SESSION_VAL = {
-    f'{s}{i}': i*3 + idx 
-        for i in [0, 1]
-        for idx, s in enumerate(['Fall', 'Winter', 'Spring']) 
-}
-
-def dfs(course: str, visited: set, prereq_dag: dict, forward_dag: dict, schedule: dict, courses_avail: dict) -> None:
-    if course in visited:
-        return
-    visited.add(course)
-
-    if prereq_dag[course]:
-        for prereq in prereq_dag[course]:
-            if prereq not in visited:
-                dfs(prereq, visited, prereq_dag, forward_dag, schedule, courses_avail)
-    else:
-        for i in [0, 1]:
-            for session in courses_avail[course]:
-                if len(schedule[f'{session}{i}']) < 4:
-                    schedule[f'{session}{i}'].append(course)
-                    return
-
-    # If the course has prerequisites
-    min_score_window = -1
-    for prereq in prereq_dag[course]:
-        for k, v in schedule.items():
-            if prereq in v:
-                min_score_window = max(min_score_window, SESSION_VAL[k])
-    # Check forwards dag
-    max_score_window = 6
-    for next_course in forward_dag[course]:
-        for k, v in schedule.items():
-            if next_course in v:
-                max_score_window = min(max_score_window, SESSION_VAL[k])
-    
-    for i in [0, 1]:
-        for session in courses_avail[course]:
-            window = SESSION_VAL[f'{session}{i}'] > min_score_window and SESSION_VAL[f'{session}{i}'] < max_score_window
-            if len(schedule[f'{session}{i}']) < 4 and window:
-                schedule[f'{session}{i}'].append(course)
-                return
             
 
 def main():
-    p = CoursePlanner('data\courses.csv')
+    transferred_courses = ['ICS 6N', 'ICS 31', 'ICS 32', 'ICS 33', 'ICS 45C', 'ICS 45J', 'ICS 46', 'ICS 51']
 
-    schedule = {k: [] for k in SESSION_VAL.keys()}
+    p = CoursePlanner(
+        data_path='data\courses.csv',
+        planned_years=2,
+        completed_courses=transferred_courses
+    )
 
     availability_list = read_csv('data\course_avail.csv')
 
-    
     courses_avail = {k: availability_list[k] for k, _ in p.course_dict.items()}
     courses_avail = {k: v for k, v in sorted(courses_avail.items(), key=lambda item: len(item[1]))}
-    visited = set()
+    
 
-
-    # Completed courses
-    completed = ['ICS 6N', 'ICS 31', 'ICS 32', 'ICS 33', 'ICS 45C', 'ICS 45J', 'ICS 46', 'ICS 51']
-
-    for course in completed:
-        visited.add(course)
-        
     # Hard picked core classes
-    schedule['Fall0'] = ['ICS 6B', 'CS 122A', 'INF 43', 'STATS 67']
-    schedule['Winter0'] = ['ICS 6D', 'ICS 139W', 'INF 101', 'INF 113']
-    schedule['Winter1'] = ['CS 161']
+    p.fixed_core_course('Fall', 0, ['ICS 6B', 'CS 122A', 'INF 43', 'STATS 67'])
+    p.fixed_core_course('Winter', 0, ['ICS 6D', 'ICS 139W', 'INF 101', 'INF 113'])
+    p.fixed_core_course('Winter', 1, ['CS 161'])
 
-    for _, v in schedule.items():
-        for course in v:
-            visited.add(course)            
+    p.build_plan(courses_avail)
 
-
-    for x, _ in courses_avail.items():
-        dfs(x, visited, p.prereq_dag, p.forward_dag, schedule, courses_avail)
-
-
-
-    print('-'*50, '\n')
-    for k, v in schedule.items():
-        print(f'{k}: {v}')
-    print()
-    print('-'*50, '\n')
+    p.display_schedule()
 
 
 if __name__ == '__main__':
--- src/planner.py
+++ src/planner.py
@@ -5,9 +5,14 @@ from dataclasses import dataclass
 @dataclass
 class CoursePlanner:
     data_path: str
+    planned_years: int
+    completed_courses: list = None
     _cdict: dict = None
     _pdag: dict = None
     _fdag: dict = None
+    _session_val: dict = None
+    _schedule: dict = None
+    _visited: set = None
 
     @property
     def course_dict(self) -> dict:
@@ -25,6 +30,18 @@ class CoursePlanner:
         self._cdict = self.__read_csv_to_dict()
         self._pdag = self.__build_pdag(self._cdict)
         self._fdag = self.__build_fdag(self._cdict)
+        self._session_val = {
+            f'{s}{i}': i*3 + idx 
+                for i in range(self.planned_years)
+                for idx, s in enumerate(['Fall', 'Winter', 'Spring']) 
+        }
+        self._schedule = {k: [] for k in self._session_val.keys()}
+
+        self._visited = set()
+        if self.completed_courses:
+            for course in self.completed_courses:
+                self._visited.add(course)
+
 
     def __read_csv_to_dict(self) -> dict:
         df = pd.read_csv(self.data_path)
@@ -55,3 +72,59 @@ class CoursePlanner:
                 dag[p].append(cid)
 
         return dag
+    
+
+    def __build_plan_dfs(self, course: str, courses_avail: dict) -> None:
+        if course in self._visited:
+            return
+        self._visited.add(course)
+
+        if self._pdag[course]:
+            for prereq in self._pdag[course]:
+                if prereq not in self._visited:
+                    self.__build_plan_dfs(prereq, courses_avail)
+        else:
+            for i in [0, 1]:
+                for session in courses_avail[course]:
+                    if len(self._schedule[f'{session}{i}']) < 4:
+                        self._schedule[f'{session}{i}'].append(course)
+                        return
+
+        # If the course has prerequisites
+        min_score_window = -1
+        for prereq in self._pdag[course]:
+            for k, v in self._schedule.items():
+                if prereq in v:
+                    min_score_window = max(min_score_window, self._session_val[k])
+        # Check forwards dag
+        max_score_window = 6
+        for next_course in self._fdag[course]:
+            for k, v in self._schedule.items():
+                if next_course in v:
+                    max_score_window = min(max_score_window, self._session_val[k])
+        
+        for i in [0, 1]:
+            for session in courses_avail[course]:
+                window = self._session_val[f'{session}{i}'] > min_score_window and self._session_val[f'{session}{i}'] < max_score_window
+                if len(self._schedule[f'{session}{i}']) < 4 and window:
+                    self._schedule[f'{session}{i}'].append(course)
+                    return
+    
+    
+    def fixed_core_course(self, semester: str, year: int, courses: list) -> None:
+        self._schedule[f'{semester}{year}'] = courses
+        for course in courses:
+            self._visited.add(course)
+    
+
+    def build_plan(self, courses_avail: dict) -> None:
+        for x, _ in courses_avail.items():
+            self.__build_plan_dfs(x, courses_avail)
+                
+                
+    def display_schedule(self) -> None:
+        print('-'*50, '\n')
+        for k, v in self._schedule.items():
+            print(f'{k}: {v}')
+        print()
+        print('-'*50, '\n')
